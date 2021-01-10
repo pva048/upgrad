65cda49b24212d33744c9fc9ebcf1ef94a2675d6
pva048 <76693529+pva048@users.noreply.github.com>
2021-01-10 17:01:54 +0530

    Assignment

---
+++ UPSTAC-Week3-assignment_updated/src/main/java/org/upgrad/upstac/UpstacApplication.java
@@ -0,0 +1,15 @@
+package org.upgrad.upstac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class UpstacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpstacApplication.class, args);
+	}
+
+
+}
